4db8c9d8c34c8200643aeceaf1d0b5a2aa12d144
vagrant <vagrant@localhost.localdomain>
2020-07-18 12:31:02 +0000

    [Finish]2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :index, :edit, :update]
+  #before_action :authenticate_user!, only: [:edit, :show, :index]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), notice: 'Signed in successfully.'
+    else
+      render "devise/sessions/new", notice: 'error'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @users = User.all
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #belongs_to :book
+  has_many :books, dependent: :destroy
+  attachment :profile_image, destroy: false
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, presence: true, uniqueness: true, length: {maximum: 20, minimum: 2}
+  validates :introduction, length: {maximum: 50}
+end
